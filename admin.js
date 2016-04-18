00500671c5e57544de7259083243c9eba2c0f766
Wolfgang Loch <wolfgang.loch@ageto.net>
2016-04-18 17:45:50 +0200

    added method tenant(), moved shop admin methods to shopAdmin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,13 @@
 "use strict";
-const JsonRestApi = require("qmerce-rest");
-const querystring = require("querystring");
-const AdminProduct = require("./product.js");
-const AdminInterfaceConfig = require("./interfaceConfig.js");
+const restApi = require("qmerce-rest");
+const ShopAdmin = require("./shopAdmin.js");
 
 module.exports = function(apiUrl, basicAuth) {
-	const server = JsonRestApi(apiUrl, basicAuth);
+	const server = restApi(apiUrl, basicAuth);
 
 	return {
-		getPriceBookSettings: () => server.getAsync("/priceBook/settings"),
-		setPriceBookSettings: (data) => server.postAsync("/priceBook/settings", data),
-
-		searchProducts: (options) => {
-			return server.getAsync("/search/product?"+querystring.stringify(options));
-		},
+		getTenants: () => server.getAsync("/tenants"),
 		
-		product: (id) => new AdminProduct(id, server),
-		
-		interfaceConfig: (interfaceId) => new AdminInterfaceConfig(interfaceId, server),
+		shop: (contextId) => new ShopAdmin(apiUrl + '/' + contextId, basicAuth)
 	}
 };
---
+++ shopAdmin.js
@@ -0,0 +1,20 @@
+"use strict";
+const JsonRestApi = require("qmerce-rest");
+const querystring = require("querystring");
+const AdminProduct = require("./product.js");
+const AdminInterfaceConfig = require("./interfaceConfig.js");
+
+module.exports = function(apiUrl, basicAuth) {
+	const server = JsonRestApi(apiUrl, basicAuth);
+
+	return {
+		getPriceBookSettings: () => server.getAsync("/priceBook/settings"),
+		setPriceBookSettings: (data) => server.postAsync("/priceBook/settings", data),
+
+		searchProducts: (options) => server.getAsync("/search/product?"+querystring.stringify(options)),
+		
+		product: (id) => new AdminProduct(id, server),
+		
+		interfaceConfig: (interfaceId) => new AdminInterfaceConfig(interfaceId, server)
+	}
+};
